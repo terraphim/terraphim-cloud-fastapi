7e662791fd46fd87800ca38c14b7bdfbdc0e79fe
AlexMikhalev <alex@metacortex.engineer>
2023-03-26 14:16:24 +0100

    Earthfly CI
    Signed-off-by: AlexMikhalev <alex@metacortex.engineer>

--- data/dataloader.py
+++ data/dataloader.py
@@ -5,5 +5,5 @@ with open('ref_arch.json', encoding='utf-8') as f:
     articles = json.loads(f.read())
 
 for article in articles:
-    r = requests.post('http://127.0.0.1:8000/article/new', json = article)
+    r = requests.post('http://api.thepattern.digital:8181/article/new', json = article)
     print(f"Created article {article['title']} with ID {r.text}")
---
+++ start.sh
@@ -0,0 +1,4 @@
+docker compose up -d 
+python3 -m venv ./venv_terraphim_cloud
+source ./venv_terraphim_cloud/bin/activate
+sudo ufw allow 8181/tcp
