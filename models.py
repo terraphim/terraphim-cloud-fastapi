ff923d5506e6191006eda09617ec69aebb259f86
AlexMikhalev <alex@metacortex.engineer>
2023-07-31 17:08:56 +0100

    New role system operator
    Signed-off-by: AlexMikhalev <alex@metacortex.engineer>

--- models.py
+++ models.py
@@ -3,28 +3,32 @@ import datetime
 from typing import List, Optional
 
 #FIXME: change to env variable
-# REDIS_DATA_URL = "redis://localhost:9001"
-host="127.0.0.1"
-port=6379
+REDIS_DATA_URL = "redis://127.0.0.1:6379"
+# host="127.0.0.1"
+# port=6379
 
 
-# class Article(HashModel):
-#     title: str = Field(index=True, full_text_search=True)
-#     url: str = Field(index=True, full_text_search=True)
-#     body: str = Field(index=True, full_text_search=True)
-#     class Meta:
-#         database = get_redis_connection(url=REDIS_DATA_URL, decode_responses=True)
-
-class Article(JsonModel):
+class Article(HashModel):
     title: str = Field(index=True, full_text_search=True)
     url: str = Field(index=True, full_text_search=True)
-    description: Optional[str] = Field(index=True, full_text_search=True)
     body: str = Field(index=True, full_text_search=True)
-    tags: Optional[List[str]] = Field(index=True)
+
     date_added: datetime.date = Field(
         default=datetime.datetime.today().strftime("%Y-%m-%d")
     )
     class Meta:
-        database = get_redis_connection(host=host,port=port, decode_responses=True)
+        database = get_redis_connection(url=REDIS_DATA_URL, decode_responses=True)
+
+# class Article(JsonModel):
+#     title: str = Field(index=True, full_text_search=True)
+#     url: str = Field(index=True, full_text_search=True)
+#     description: Optional[str] = Field(index=True, full_text_search=True)
+#     body: str = Field(index=True, full_text_search=True)
+#     tags: Optional[List[str]] = Field(index=True)
+#     date_added: datetime.date = Field(
+#         default=datetime.datetime.today().strftime("%Y-%m-%d")
+#     )
+#     class Meta:
+#         database = get_redis_connection(url=REDIS_DATA_URL, decode_responses=True)
 
 Migrator().run()
